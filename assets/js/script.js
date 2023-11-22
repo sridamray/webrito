33981aec8f13bb42b637e12baa27df54725abccd
Sridam Roy <sridamroybd@gmail.com>
2023-11-22 21:42:13 +0600

    webrito Markup complete

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+(function($){
+	'use script';
+
+
+
+    // brand_service_slider
+    $('.brand_service_slider').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        items:4,
+        
+    })
+
+    // Faq Javascript
+
+    function toggleContent(h2) {
+        h2.classList.toggle("show-content");
+    }
+
+
+
+
+	
+
+
+
+})();
+	

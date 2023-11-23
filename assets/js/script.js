5e683fde9374f7411d4af48e745583e03117f527
Sridam Roy <sridamroybd@gmail.com>
2023-11-24 00:28:47 +0600

    5 Section Design Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,63 @@
+(function ($) {
+	"use strict";
 
-(function($){
-	'use script';
+	///////////////////////////////////////////////////
+	// Sticky Header Js
+	var windowOn = $(window)
+	windowOn.on('scroll', function () {
+		var scroll = windowOn.scrollTop();
+		if (scroll < 400) {
+			$("#header-sticky").removeClass("header-sticky");
+		} else {
+			$("#header-sticky").addClass("header-sticky");
+		}
+	})//
+
+	//Slider One
+
+	$('.brand_service_slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		autoplay:true,
+		autoplaySpeed:5000,
+		arrows:false,
+		pauseOnFocus:false,
+		pauseOnHover:false,
+		responsive: [
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 3,
+			  slidesToScroll: 3,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 600,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		  // You can unslick at a given breakpoint now by adding:
+		  // settings: "unslick"
+		  // instead of a settings object
+		]
+	  });
+					  
 
 
 
-    // brand_service_slider
-    $('.brand_service_slider').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        items:4,
-        
-    })
 
-    // Faq Javascript
-
-    function toggleContent(h2) {
-        h2.classList.toggle("show-content");
-    }
-
-
-
-
-	
-
-
-
-})();
-	
+})(jQuery);
